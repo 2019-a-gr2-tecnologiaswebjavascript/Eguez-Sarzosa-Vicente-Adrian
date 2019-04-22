8197c62736e41c9d4fd9cfe31a5140142c90cb2b
Adrian Eguez <eadepto@hotmail.com>
2019-04-22 17:23:59 -0500

    cambios en ejercicio

--- 01-Angular/galeria/src/app/servicios/carrito/carrito.service.ts
+++ 01-Angular/galeria/src/app/servicios/carrito/carrito.service.ts
@@ -1,6 +1,39 @@
 import { Injectable } from '@angular/core';
+import { ItemCarritoCompras } from '../../interfaces/item-carrito-compras';
 
 @Injectable()
 export class CarritoService {
     carritoCompras = [];
+
+    agregarCarritoDeCompras(
+        itemCarrito:ItemCarritoCompras
+        ):ItemCarritoCompras[]{
+        const identificador = itemCarrito.valor;
+        
+        // [1,2,3].find()
+
+        
+        
+        return [];
+    }
+
+
 }
+
+[1,2,3,4].forEach(busqueda);
+
+
+function busqueda(valor, indice, arreglo){
+    console.log('Valor:',valor);
+    if(valor==1){
+        console.log('Te encontre 1 !!');
+    }
+}
+
+
+
+
+
+
+
+
