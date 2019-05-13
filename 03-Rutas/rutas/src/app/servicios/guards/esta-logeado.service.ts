d893d83494dd906d5e499b4dba25c97a3d2832b8
Adrian Eguez <eadepto@hotmail.com>
2019-05-13 18:00:28 -0500

    login

--- 03-Rutas/rutas/src/app/servicios/guards/esta-logeado.service.ts
+++ 03-Rutas/rutas/src/app/servicios/guards/esta-logeado.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstaLogeadoService implements CanActivate {
-  constructor() { }
+  constructor(private readonly _authService:AuthService,
+              private readonly _router:Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, 
               state: RouterStateSnapshot):boolean {
-                return true;
+                const estaLogeado = this._authService.estaLogeado;
+                if(estaLogeado){
+                  console.log('Bienvenido');
+                  return true;
+                } else {
+                  const url = ['/home','app'];
+                  this._router.navigate(url);
+                  console.log('No tiene permisos');
+                  return false;
+                }
+                
   }
 
 }
