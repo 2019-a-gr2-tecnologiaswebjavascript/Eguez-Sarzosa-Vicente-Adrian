fa11aba33ca1258e5008cb5e3307780fc62b8cd8
Adrian Eguez <eadepto@hotmail.com>
2019-05-17 18:01:40 -0500

    formularios

--- 04-Formularios/formularios/src/app/app.component.ts
+++ 04-Formularios/formularios/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formularios';
+  nombre: string;
+  apellido: string;
+  q: string;
+
+  guardarUsuario(formulario){
+    console.log(formulario.controls.nombreUsuario.value);
+    console.log(this.nombre);
+    // this.nombre = "Benito C."
+  }
 }
