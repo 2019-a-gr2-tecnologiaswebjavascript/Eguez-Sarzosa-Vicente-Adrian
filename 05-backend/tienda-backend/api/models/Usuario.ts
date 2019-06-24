6de2de50ffb64e62b544e27a0956e5866c478142
Adrian Eguez <eadepto@hotmail.com>
2019-06-24 16:35:13 -0500

    modelo usuario

--- 05-backend/tienda-backend/api/models/Usuario.ts
+++ 05-backend/tienda-backend/api/models/Usuario.ts
@@ -9,11 +9,47 @@ module.exports = {
 
   attributes: {
     nombre: {
-      type: 'string'
+      type: 'string',
+      required: true,
+      minLength: 3,
+      maxLength: 60
     },
     apellido: {
       type: 'string',
       required: true
+    },
+    cedula: {
+      type: 'string',
+      required: true,
+      unique: true,
+      minLength: 10,
+      maxLength: 25
+    },
+    username:{
+      type:'string',
+      required:true,
+      unique:true,
+      minLength: 3,
+      maxLength: 60,
+    },
+    sueldo:{
+      type: 'number',
+      min: 100,
+      max: 5000.00,
+      defaultsTo: 100
+    },
+    estaCasado:{
+      type: 'boolean',
+      defaultsTo: false
+    },
+    tipoUsuario:{
+      type: 'string',
+      enum: ['normal','pendiente','premium'],
+      defaulstTo: 'normal'
+    },
+    correo:{
+      type:'string',
+      isEmail: true
     }
   },
 
